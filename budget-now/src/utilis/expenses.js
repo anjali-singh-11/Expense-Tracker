1133335821624677693b325b1b00633ea0aaf024
anjali-singh-11 <anjali4628@gmail.com>
2026-05-04 22:14:26 +0530

    updated eepenses and expense date logic

--- budget-now/src/utilis/expenses.js
+++ budget-now/src/utilis/expenses.js
@@ -31,3 +31,44 @@ export const getExpensesByCategory = (expenses) => {
     });
 };
 
+export const getTotalExpenses = (expenses) => {
+    return expenses.reduce((total, expense) => total + expense.amount, 0);
+};
+
+export const getCharData = (expenses) => {
+    const expensesByCategory = getExpensesByCategory(expenses);
+    return Object.entries(expensesByCategory)
+    .filter(([_, value]) => value > 0)
+    .map(([name, value]) => ({
+        name:name.charAt(0).toUpperCase() + name.slice(1),
+        value,
+    }));
+};
+
+export const getCategoryTextColor = (category) => {
+    const colors = {
+        food: "text-indigo-500",
+        transport: "text-cyan-500",
+        entertainment: "text-purple-500",
+        utilities: "text-teal-500",
+        health: "text-green-500",
+        shopping: "text-orange-500",
+        other: "text-slate-500",
+    };
+    return colors[category] || "text-gray-500";
+};
+
+export const getMonthName = (date) => {
+    return date.toLocateString("default", { month: "long" });
+};
+
+export const getExpensesByMonth = (expenses, numMonths = 6) => {
+    const now = new Date();
+    const result= {};
+
+    for (let i = 0; i < numMonths; i++) {
+        const d = new Date(now.getFullYear(), now.getMonth() - i, 1);
+        const monthYear = `${getMonthName(d)} ${d.getFullYear()}`;
+        result[monthYear] = 0;
+    }
+};
