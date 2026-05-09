beb7e511e3f7f7146fccda67d8eea06b327b2a2c
anjali-singh-11 <anjali4628@gmail.com>
2026-05-09 18:32:27 +0530

    add file ExpenseChart

--- budget-now/src/components/Dashboard.jsx
+++ budget-now/src/components/Dashboard.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import ExpenseSummary from './ExpenseSummary'
+import React from 'react';
+import ExpenseSummary from './ExpenseSummary';
+import ExpenseChart from './ExpenseChart';
 
 const Dashboard = () => {
   return (
     <div>
+      {/* exapense summary */}
       <ExpenseSummary />
-        {/* exapense summary */}
-        {/* exapense summary */}
+
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
+        <div>
+          <ExpenseChart />
+        </div>
+        <div>{/* expense form */}</div>
+      </div>
+      {/* exapense summary */}
     </div>
   );
 };
--- budget-now/src/components/ExpenseSummary.jsx
+++ budget-now/src/components/ExpenseSummary.jsx
@@ -5,7 +5,7 @@ import {
   getTotalExpenses,
   getExpensesByCategory,
 } from "../utilis/expenses";
-import { Wallet } from "lucide-react";
+import { TrendingDown, TrendingUp, Wallet } from "lucide-react";
 
 const ExpenseSummary = () => {
   const { expenses } = useExpenses();
@@ -24,7 +24,7 @@ const ExpenseSummary = () => {
     }
   });
 
-  return;
+  return (
   <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
     <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg">
       <div className="flex items-center space-x-4">
@@ -42,7 +42,7 @@ const ExpenseSummary = () => {
     <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg">
       <div className="flex items-center space-x-4">
         <div className="bg-red-100 p-3 rounded-full">
-          <Wallet size={24} className="text-red-500" />
+          <TrendingUp size={24} className="text-red-500" />
         </div>
         <div>
           <h3 className="text-sm font-medium text-gray-500">
@@ -51,8 +51,8 @@ const ExpenseSummary = () => {
           <p className="text-2xl font-bold text-expense-dark">
             {highestCategory.name !== "none" ? (
               <>
-                <span>{highestCategory.name}</span>
-                <span>({formatCurrency(highestCategory.amountmount)})</span>
+                <span className="capitalize">{highestCategory.name}</span>
+                <span className="text-sm font-normal text-gray-500 ml-2">({formatCurrency(highestCategory.amount)})</span>
               </>
             ) : (
               "None"
@@ -61,9 +61,21 @@ const ExpenseSummary = () => {
         </div>
       </div>
     </div>
-    <div></div>
-    <div></div>
-  </div>;
+    <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg">
+        <div className="flex items-center space-x-4">
+          <div className="bg-green-100 p-3 rounded-full">
+            <TrendingDown size={24} className="text-green-500" />
+          </div>
+          <div>
+            <h3 className="text-sm font-medium text-gray-500">Total Entries</h3>
+            <p className="text-2xl font-bold text-expense-dark">
+              {expenses.length}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );  
 };
 
 export default ExpenseSummary;
